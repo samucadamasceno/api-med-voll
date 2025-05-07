de1c77d504ba96553807b182c96e64e75ca5fd75
Samuel Damasceno <smkdo04@gmail.com>
2025-05-07 15:24:23 -0300

    Creating medico controller

---
+++ src/main/java/med/voll/api/Controller/MedicoController.java
@@ -0,0 +1,15 @@
+package med.voll.api.Controller;
+
+import med.voll.api.medico.DadosCadastroMedico;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+        
+    }
+
+}
